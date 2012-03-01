cf2e466052aade26ec8082afd79bd3467ab80385
Dan De Luca <dan.deluca@newicon.net>
2012-03-01 23:38:32 +0000

    tabs css

--- app/views/transactions/create.php
+++ app/views/transactions/create.php
@@ -15,23 +15,25 @@ Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl . '/js/bootstra
 
 <div class="row-fluid">
 	<h1>Create a Transaction</h1>
-    <ul class="nav nav-tabs">
-		<li class="active"><a href="#withdrawal" data-toggle="tab">Withdrawal</a></li>
-		<li><a href="#deposit" data-toggle="tab">Deposit</a></li>
-		<li><a href="#transfer" data-toggle="tab">Transfer</a></li>
-    </ul>
-    <div class="tab-content">
-		<div class="tab-pane active" id="withdrawal">
-			<h3>Withdrawal</h3>
-			<?php echo $this->renderPartial('_form', array('model' => $model)); ?>	
-		</div>
-		<div class="tab-pane" id="deposit">
-			<h3>Deposit</h3>
-			<?php echo $this->renderPartial('_form', array('model' => $model)); ?>	
-		</div>
-		<div class="tab-pane" id="transfer">
-			<h3>Transfer</h3>
-			<?php echo $this->renderPartial('_form', array('model' => $model)); ?>	
-		</div>
-    </div>
+	<div class="form-tabs">
+		<ul class="nav nav-tabs">
+			<li class="active"><a href="#withdrawal" data-toggle="tab">Withdrawal</a></li>
+			<li><a href="#deposit" data-toggle="tab">Deposit</a></li>
+			<li><a href="#transfer" data-toggle="tab">Transfer</a></li>
+		</ul>
+		<div class="tab-content">
+			<div class="tab-pane active" id="withdrawal">
+				<h3>Withdrawal</h3>
+				<?php echo $this->renderPartial('_form', array('model' => $model)); ?>	
+			</div>
+			<div class="tab-pane" id="deposit">
+				<h3>Deposit</h3>
+				<?php echo $this->renderPartial('_form', array('model' => $model)); ?>	
+			</div>
+			<div class="tab-pane" id="transfer">
+				<h3>Transfer</h3>
+				<?php echo $this->renderPartial('_form', array('model' => $model)); ?>	
+			</div>
+		</div>		
+	</div>
 </div>
